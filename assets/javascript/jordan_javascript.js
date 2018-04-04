c235c6b2d91b41d9a599052aacdd09ed659d931a
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-04-03 19:03:43 -0500

    fixing merge conflicts

--- assets/javascript/jordan_javascript.js
+++ assets/javascript/jordan_javascript.js
@@ -3,16 +3,7 @@ $("document").ready(function() {
 	$(document).ready(function(){
 
 	$('#date').datepicker();
-<<<<<<< HEAD
 	$('.modal').modal();
   	});
 })
 
-=======
-	  });
-	  
-	  var pickedDate = $("input")
-
-	  console.log(pickedDate);
-})
->>>>>>> 28326544ee6bb5c43543c6914d66562d452e4f0e
