44390d8faec4d8b03859a2b1b179d25d0bf724f3
Guillaume Paugam <guipaugam@icloud.com>
2018-04-06 11:03:09 -0500

    Worked on displaying the events better

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -7,7 +7,9 @@ $("#search").on("click", function (event) {
     event.preventDefault();
     // console.log("Entered date: " + $("#date").val())
     var dateEntered = $("#date").val();
-    var nextDay = moment().add(1, 'day');
+    var nextDay = moment(dateEntered).add(1, 'day');
+    console.log("nextDay before reformatting:" + nextDay);
+    
     var reformattedDate = moment(dateEntered).format(dateFormat);
     var reformattedNextDay = moment(nextDay).format(dateFormat);
     // console.log("reformattedDate: " + reformattedDate);
@@ -46,12 +48,17 @@ $.ajax({
     const { event } = responseJSON.events;
 
     for (var i = 0; i < event.length; i++) {
-        const description = event[i].description ? event[i].description : "No Description Available.";
-        const venueAddress = event[i].venue_address ? event[i].venue_address : "No Address Available.";
+        const description = event[i].description ? event[i].description : "No Description yet. Come check later!";
+        // const venueAddress = event[i].venue_address ? event[i].venue_address : "No Address Available.";
 
-        const eventInfo = $("<tr>").html("<td><strong>" + event[i].title + "</strong></td><td>" + description + "</td><td> ") 
+        const eventInfo = $("<tr>").html("<td class='place for heart'></td> <td class='title'> <strong>" + event[i].title + "</strong></td><td class='description'>" + description + "</td><td class='date'>" + event[i].start_time) 
         // event[i].venue_name + "</td><iframe width='600' height='450' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyDwCKEyy5bmqNtKpj_zaIYN8QL-DSE0DAc&q=" + 
         // response.events.venue_name + " allowfullscreen'></iframe><td>" + event[i].start_time + "</td>**********************<br>");
+        // 
+        //  <th>Event Title</th>
+        // <th>Event Description</th>
+        // <th>Date</th>
+        // <th>Time</th>
 
         $("#event-list").append(eventInfo);
     }
