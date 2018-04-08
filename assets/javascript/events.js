240ebd7280f61b1d5ed3c2c6a96bd615f31d8346
Guillaume Paugam <guipaugam@icloud.com>
2018-04-08 09:29:22 -0500

    Added Jordan's code to favorite events

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -1,28 +1,22 @@
+// global variables
+const eventsArray = [];
+const favIcon = "favorite";
+const unFavIcon = "favorite_border";
+
+let timeStamp = Math.floor(Date.now() / 1000)
+console.log("timeStamp: " , timeStamp)
+
+// THis is the time format the event API is requesting  
+const dateFormat = "YYYYMMDD"; 
 
 $(document).ready(function(){
     $('.datepicker').datepicker();
   });
 
-
-var timeStamp = Math.floor(Date.now() / 1000)
-console.log("timeStamp: " , timeStamp)
-
-// THis is the time format the API is requesting  
-
-var dateFormat = "YYYYMMDD"; 
-
+// Functions triggered on event search
 
 $("#search").on("click", function (event) {
     event.preventDefault();
-    
-
-    // var formattedDate = moment($("#date").val()).format(dateFormatWeWant)
-        
-    // // add 00 because that's how the API wants us to structure the date in our query
-    // var fromDate = formattedDate + "00"; 
-    // var toDate = formattedDate + "00"; 
-    // console.log("fromDate: " + fromDate);
-    // console.log("toDate: " + toDate);
 
     var userCity = $("#location").val().trim();
     console.log("userCity:", userCity);
@@ -37,21 +31,11 @@ $("#search").on("click", function (event) {
     var fromDate = reformattedDateEntered + "00"; 
     var toDate = reformattedDateEntered + "00";
 
-
-var userCity = $("#location").val().trim();
-console.log("userCity:", userCity);
-
-
-
 var eventAPIKey = "R2SmVPVrHGFhKdGX";
 var eventsURL = "http://api.eventful.com/json/events/search?...&date=" + fromDate + "-" + toDate + "&page_size=10&location=" +
  userCity + "&app_key=" + eventAPIKey;
 
 // output format to JSON see: http://api.eventful.com/docs/formats 
-
-var eventsArray = [];
-var favIcon = "favorite";
-var unFavIcon = "favorite_border";
 
 $.ajax({
     url: eventsURL,
@@ -68,12 +52,11 @@ $.ajax({
             var description = e.description ? e.description : "No Description yet. Come check later!";
 
             // This is an object constructor to create each event retrieved from the API as an object
-            function EventDisplayed(id, icon, favIcon, unFavIcon, status, title, description, start) {
+            function EventDisplayed(id, icon, favIcon, heartStatus, title, description, start) {
                 this.id = id;
                 this.icon = icon;
                 this.favIcon = favIcon;
-                this.unFavIcon = unFavIcon;
-                this.status = status;
+                this.heartStatus = heartStatus;
                 this.title = title;
                 this.description = description;
                 this.start = start;
@@ -83,10 +66,9 @@ $.ajax({
             // and this is how objects can be dynamically created with properties of the constructor and elements of the API + the "for loop"
             var newEvent = new EventDisplayed(
                 "id='" + e.id + "'>",
-                "<td class='icon'> <i class='material-icons'>" + unFavIcon + "</i>",
+                "<td class='icon'> <i heart='empty' class='material-icons' id='favorite-event'>" + unFavIcon + "</i>",
                 "favorite",
-                "favorite_border",
-                false,
+                "empty",
                 e.title,
                 description,
                 moment(e.start_time).format("M/DD H:mm A"),
@@ -95,12 +77,12 @@ $.ajax({
                 var eInfo = "<tr class='event'"; 
                 var idTag = "<td id='obj-"; 
                 // var iconTag = "<td class='icon'> <i class='material-icons'>" + newEvent.icon
-                var eEmphasis = "<td class='event-title'>"
+                var eTitle = "<td class='event-title'>"
                 var eDescription = "<td class ='event-description'>"
                 var eDate = "<td class='date'>"
                 
                 // and here I create the row with the attributes of the object + the HTML tags
-                var eRow = `${eInfo} ${newEvent.id} ${newEvent.icon} ${eEmphasis} ${newEvent.title} ${eDescription} ${newEvent.description} ${eDate} ${newEvent.start}`;
+                var eRow = `${eInfo} ${newEvent.id} ${newEvent.icon} ${eTitle} ${newEvent.title} ${eDescription} ${newEvent.description} ${eDate} ${newEvent.start}`;
                 console.log(eRow);
 
             // Youngmi's code breaking:   
@@ -116,4 +98,9 @@ $.ajax({
             $("#events").append(eRow);
         }
     });
+});
+
+$(document).on("click", ".material-icons", function (){
+    getId = ($(this).attr("id"))
+    console.log(getId);
 });
